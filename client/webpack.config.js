ce3e2188b5b54fa27db0d4a8fffb47adf838b44e
Ruxin Qu <rosheenqu@gmail.com>
2022-12-26 21:12:16 -0800

    fix the bug: now the localstorage works properly

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -29,9 +29,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'JATE',
+        short_name: 'Just Another Text Editor',
+        description: 'text editor that works online and offline',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
